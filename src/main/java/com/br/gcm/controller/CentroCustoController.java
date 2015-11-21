bfce69cab4ad62cd889994a3aebfd5b5932b9e9f
João Carlos <joaocarlosdv@hotmail.com>
2015-11-21 00:55:14 -0300

    .
    .

--- src/main/java/com/br/gcm/controller/CentroCustoController.java
+++ src/main/java/com/br/gcm/controller/CentroCustoController.java
@@ -37,20 +37,33 @@ public class CentroCustoController {
     @Inject private Rotinas rotinas;
     @Inject private EmpresaDao empresaDao;
 
-    private String mensagem = "";
-    private int tipo = 9;
-
-    private  void limparmensagem(){
-        mensagem = "";
-        tipo = 9;
-    }
-
     //Filtros
     @RequestMapping(value = "/centrocusto_lista", method = RequestMethod.POST)
     public ModelAndView filtros(@ModelAttribute CentroCusto filtros, @PageableDefault(size = 10) Pageable pageable) {
         ModelAndView mav = new ModelAndView();
         Usuario usuario = rotinas.usuarioLogado();
         List<Empresa> listaEmpresa = empresaDao.selectEmpresasUsuario(usuario.getId_usuario());
+
+        if (!listaEmpresa.isEmpty()){
+            filtros.setId_Empresa(listaEmpresa.get(0).getId_Empresa());
+        };
+
+        Boolean lista = rotinas.validaTransacaoUsuario(usuario.getId_usuario(), "1501");
+        if (lista != true) {
+            mav.addObject("mensagem", "AVISO: Transação não permitida.");
+            mav.setViewName("mensagemerro");
+            return mav;
+        }
+        Boolean novo     = rotinas.validaTransacaoUsuario(usuario.getId_usuario(), "150101");
+        Boolean editar   = rotinas.validaTransacaoUsuario(usuario.getId_usuario(), "150102");
+        Boolean deletar  = rotinas.validaTransacaoUsuario(usuario.getId_usuario(), "150103");
+        Boolean detalhes = rotinas.validaTransacaoUsuario(usuario.getId_usuario(), "150104");
+
+
+        mav.addObject("novo", novo);
+        mav.addObject("editar", editar);
+        mav.addObject("deletar", deletar);
+        mav.addObject("detalhes", detalhes);
 
         mav.addObject("filtros", filtros);
         mav.addObject("listaempresa", listaEmpresa);
@@ -71,6 +84,21 @@ public class CentroCustoController {
             filtros.setId_Empresa(listaEmpresa.get(0).getId_Empresa());
         }
 
+        Boolean lista = rotinas.validaTransacaoUsuario(usuario.getId_usuario(), "1501");
+        if (lista != true) {
+            model.addAttribute("mensagem", "AVISO: Transação não permitida.");
+            return "mensagemerro";
+        }
+        Boolean novo     = rotinas.validaTransacaoUsuario(usuario.getId_usuario(), "150101");
+        Boolean editar   = rotinas.validaTransacaoUsuario(usuario.getId_usuario(), "150102");
+        Boolean deletar  = rotinas.validaTransacaoUsuario(usuario.getId_usuario(), "150103");
+        Boolean detalhes = rotinas.validaTransacaoUsuario(usuario.getId_usuario(), "150104");
+
+        model.addAttribute("novo", novo);
+        model.addAttribute("editar", editar);
+        model.addAttribute("deletar", deletar);
+        model.addAttribute("detalhes", detalhes);
+
         model.addAttribute("filtros", filtros);
         model.addAttribute("listaempresa", listaEmpresa);
         model.addAttribute("lista", centroCustoDao.selectAll_paginado(filtros.getId_Empresa() , pageable));
@@ -80,12 +108,19 @@ public class CentroCustoController {
 
     //Deletar
     @RequestMapping(value = "/centrocusto_deleta/{id}")
-    public String deletar(@PathVariable("id") Integer id) {
+    public String deletar(@PathVariable("id") Integer id, Model model) {
+        Usuario usuario = rotinas.usuarioLogado();
+        Boolean lista = rotinas.validaTransacaoUsuario(usuario.getId_usuario(), "150103");
+        if (lista != true) {
+            model.addAttribute("mensagem", "AVISO: Transação não permitida.");
+            return "mensagemerro";
+        }
+
         try{
             centroCustoService.delete(id);
         }catch(Exception e){
-            JOptionPane JOptinPane = new JOptionPane();
-            JOptinPane.showMessageDialog(null,e.getCause().toString(),"Alerta", JOptionPane.INFORMATION_MESSAGE);
+            model.addAttribute("mensagem", e.getCause().getMessage().toString());
+            return "mensagemerro";
         }
         return "redirect:/centrocusto_lista";
     }
@@ -94,6 +129,12 @@ public class CentroCustoController {
     @RequestMapping(value = "/centrocusto_novo", method = RequestMethod.GET)
     public String novo(ModelMap model) {
         Usuario usuario = rotinas.usuarioLogado();
+        Boolean lista = rotinas.validaTransacaoUsuario(usuario.getId_usuario(), "150101");
+        if (lista != true) {
+            model.addAttribute("mensagem", "AVISO: Transação não permitida.");
+            return "mensagemerro";
+        }
+
         List<Empresa> listaEmpresa = empresaDao.selectEmpresasUsuario(usuario.getId_usuario());
 
         CentroCusto centroCusto = new CentroCusto();
@@ -104,12 +145,12 @@ public class CentroCustoController {
 
     //Insert
     @RequestMapping(value = "/centrocusto_insert", method = RequestMethod.POST)
-    public String insert(@ModelAttribute CentroCusto centroCusto) {
+    public String insert(@ModelAttribute CentroCusto centroCusto, Model model) {
         try{
             centroCustoService.insert(centroCusto);
         }catch(Exception e){
-            JOptionPane JOptinPane = new JOptionPane();
-            JOptinPane.showMessageDialog(null,e.getCause().toString(),"Alerta", JOptionPane.INFORMATION_MESSAGE);
+            model.addAttribute("mensagem", e.getCause().getMessage().toString());
+            return "mensagemerro";
         }
         return "redirect:/centrocusto_lista";
     }
@@ -118,6 +159,12 @@ public class CentroCustoController {
     @RequestMapping(value = "/centrocusto_editar/{id}", method = RequestMethod.GET)
     public String editar(@PathVariable("id") Integer id, Model model) {
         Usuario usuario = rotinas.usuarioLogado();
+        Boolean lista = rotinas.validaTransacaoUsuario(usuario.getId_usuario(), "150102");
+        if (lista != true) {
+            model.addAttribute("mensagem", "AVISO: Transação não permitida.");
+            return "mensagemerro";
+        }
+
         List<Empresa> listaEmpresa = empresaDao.selectEmpresasUsuario(usuario.getId_usuario());
 
         model.addAttribute("listaempresa", listaEmpresa);
@@ -128,12 +175,12 @@ public class CentroCustoController {
 
     //Update
     @RequestMapping(value = "/centrocusto_update", method = RequestMethod.POST)
-    public String update(@ModelAttribute CentroCusto centroCusto) {
+    public String update(@ModelAttribute CentroCusto centroCusto, Model model) {
         try{
             centroCustoService.update(centroCusto);
         }catch(Exception e){
-            JOptionPane JOptinPane = new JOptionPane();
-            JOptinPane.showMessageDialog(null,e.getCause().toString(),"Alerta", JOptionPane.INFORMATION_MESSAGE);
+            model.addAttribute("mensagem", e.getCause().getMessage().toString());
+            return "mensagemerro";
         }
         return "redirect:/centrocusto_lista";
     }
@@ -141,6 +188,12 @@ public class CentroCustoController {
     @RequestMapping(value = "/centrocusto_detalhes/{id}", method = RequestMethod.GET)
     public String detalhes(@PathVariable("id") Integer id, Model model) {
         Usuario usuario = rotinas.usuarioLogado();
+        Boolean lista = rotinas.validaTransacaoUsuario(usuario.getId_usuario(), "150104");
+        if (lista != true) {
+            model.addAttribute("mensagem", "AVISO: Transação não permitida.");
+            return "mensagemerro";
+        }
+
         List<Empresa> listaEmpresa = empresaDao.selectEmpresasUsuario(usuario.getId_usuario());
 
         model.addAttribute("listaempresa", listaEmpresa);
